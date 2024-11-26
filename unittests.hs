0a3dcb093969b330f5335375dc2d28b7748896ad
ineslelin <ines.l.lintner@gmail.com>
2024-11-26 11:43:51 +0100

    test insert string/int not working yet

--- unittests.hs
+++ unittests.hs
@@ -1,10 +1,34 @@
-module Unittests where
+module Main where
 
 import Test.HUnit
-import Main
+import RBT
 
 testTokenize :: Test
-testTokenize = TestCase (assertEqual ["this", "should", "be", "tokenized"] (tokenizeText "This should be tokenized"))
+testTokenize = TestCase (assertEqual "Should tokenize correctly" 
+                            ["This", "should", "be", "tokenized"] 
+                            (tokenizeText "This should be tokenized"))
+
+testClean :: Test
+testClean = TestCase (assertEqual "Should clean text" 
+                        "allcaps" 
+                        (cleanText "ALLCAPS"))
+
+-- testInsertString :: Test
+-- testInsertString = TestCase (assertEqual "Insert string"
+--                                 (Node Black Empty "String" Empty)
+--                                 (insert "String" (Empty :: Tree String)))
+
+-- testInsertInt :: Test
+-- testInsertInt = TestCase (assertEqual "Insert integer"
+--                                 (Node Black Empty 2 Empty)
+--                                 (insert 2 (Empty :: Tree Integer)))
+
+tests :: Test
+tests = TestList [TestLabel "Test Tokenize" testTokenize,
+                  TestLabel "Test Clean" testClean,
+                --   TestLabel "Test Insert String" testInsertString,
+                --   TestLabel "Test Insert Integer" testInsertInt
+                 ]
 
 main :: IO Counts
 main = runTestTT tests
